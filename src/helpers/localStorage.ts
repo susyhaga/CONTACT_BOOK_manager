54427e0da6e46241a9909e779f5920e7000a5f12
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-10-05 19:38:44 -0300

    Update localStorage.ts

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,10 +1,13 @@
-import { ContactModel } from '../components/Contact'
+// Funções para gerenciar contatos no localStorage
+import { ContactModel } from '../components/Contact';
 
+// Salva contatos no localStorage
 export const saveContactsToLocalStorage = (contacts: ContactModel[]) => {
-  localStorage.setItem('contacts', JSON.stringify(contacts))
-}
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+};
 
+// Obtém contatos do localStorage
 export const getContactsFromLocalStorage = (): ContactModel[] => {
-  const contacts = localStorage.getItem('contacts')
-  return contacts ? JSON.parse(contacts) : []
-}
+  const contacts = localStorage.getItem('contacts');
+  return contacts ? JSON.parse(contacts) : [];
+};
