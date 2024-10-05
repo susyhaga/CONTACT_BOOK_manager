eb5810219e06dd197c18c19a77525b59f34e402d
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-10-05 19:32:27 -0300

    Add files via upload

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import filterReducer from './slices/filters'
-import contactsReducer from './slices/contact'
-import { contactsApi } from '../services/api'
+import { configureStore } from '@reduxjs/toolkit';
+import filterReducer from './slices/filters';
+import contactsReducer from './slices/contact';
 
 const store = configureStore({
   reducer: {
     filter: filterReducer,
     contacts: contactsReducer,
-    [contactsApi.reducerPath]: contactsApi.reducer
+    // Removi a parte da API, pois você não está usando mais
+    // [contactsApi.reducerPath]: contactsApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(contactsApi.middleware)
-})
+    getDefaultMiddleware() // Não é necessário adicionar middleware da API
+    // .concat(contactsApi.middleware)
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export default store
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
