9618c6660c9f4c82cde6434d11a0a46e5e46a6b9
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-10-05 15:05:47 -0300

    Add files via upload

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { ContactModel } from '../components/Contact'
 
-const baseUrl = 'http://192.168.0.57:3001'
+const baseUrl = 'http://192.168.0.57:3001/contacts'
 
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
